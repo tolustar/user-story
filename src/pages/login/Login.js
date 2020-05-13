83d3e81713b0216dbdab2bc3291930ce347e1912
tolustar <tolustar29@gmail.com>
2020-05-13 20:36:20 +0100

    initialized project

---
+++ src/pages/login/Login.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./Login.css"
+
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="bg-teal">
+        <h2>User Story</h2>
+      </div>
+
+      <div className="form-container">
+          <form>
+            <label htmlFor="email"></label>
+          </form>
+      </div>
+
+    </div>
+  )
+}
